a3e92efc32dd0a82efb7d40ab24a8c373d1a4e44
Ryan Carr <rycarr@gmail.com>
2018-07-07 17:27:52 -0500

    Simplified while loop

--- SumDigits/src/SumDigits.java
+++ SumDigits/src/SumDigits.java
@@ -34,7 +34,7 @@ public class SumDigits
             while(number > 0)
             {
                 sum += number % 10;
-                number = number / 10;
+                number /= 10;
             }
         }
 
