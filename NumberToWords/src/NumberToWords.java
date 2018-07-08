7baa022a60678e7834900d08f96d65c4f177e225
Ryan Carr <rycarr@gmail.com>
2018-07-08 16:47:32 -0500

    Added NumberToWords method

--- NumberToWords/src/NumberToWords.java
+++ NumberToWords/src/NumberToWords.java
@@ -29,6 +29,14 @@ public class NumberToWords
         printResults(reverse(1234) == 4321);
         printResults(reverse(100) == 1);
         System.out.println("------------------------");
+
+        System.out.println("Testing numberToWords()");
+        System.out.println("------------------------");
+        numberToWords(123);
+        numberToWords(1010);
+        numberToWords(1000);
+        numberToWords(-12);
+        System.out.println("------------------------");
     }
 
     /**
@@ -87,6 +95,81 @@ public class NumberToWords
             result *= -1;
 
         return result;
+    }
+
+    /**
+     * Converts an integer between 0 and 9 to its word form
+     * @param number Number between 0 and 9
+     */
+    public static String NumberInWord(int number)
+    {
+        String result = "";
+
+        switch(number)
+        {
+            case 0:
+                result += "Zero";
+                break;
+            case 1:
+                result += "One";
+                break;
+            case 2:
+                result += "Two";
+                break;
+            case 3:
+                result += "Three";
+                break;
+            case 4:
+                result += "Four";
+                break;
+            case 5:
+                result += "Five";
+                break;
+            case 6:
+                result += "Six";
+                break;
+            case 7:
+                result += "Seven";
+                break;
+            case 8:
+                result += "Eight";
+                break;
+            case 9:
+                result += "Nine";
+                break;
+        }
+        return result;
+    }
+
+    /**
+     * Convert a number into word form representing each digit
+     * @param number Number to be converted
+     */
+    public static void numberToWords(int number)
+    {
+        String result = "";
+        int digits = getDigitCount(number);
+        number = reverse(number);
+        int temp = number;
+
+        if(number > MIN)
+        {
+            while (number > MIN)
+            {
+                result += NumberInWord(number % 10) + " ";
+                number /= 10;
+            }
+
+            if(digits > getDigitCount(temp))
+                for(int i = getDigitCount(temp); i < digits;i++)
+                    result += NumberInWord(0) + " ";
+        }
+        else
+            result = "Invalid Value";
+
+        result = result.trim();
+
+        System.out.println(result);
     }
 
     /**
